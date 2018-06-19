383dfd4b49118c2e459add7fdd94732c2b96e8ea
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-06-19 09:55:49 +0200

    cache fonts

--- vue.config.js
+++ vue.config.js
@@ -26,5 +26,21 @@ module.exports = {
       .rule("svg")
       .test(() => false)
       .use("file-loader");
+  },
+
+  pwa: {
+    name: "My App",
+    themeColor: "#4DBA87",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+
+    // configure the workbox plugin
+    workboxPluginMode: "InjectManifest",
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: "src/sw.js"
+      // ...other Workbox options...
+    }
   }
 };
