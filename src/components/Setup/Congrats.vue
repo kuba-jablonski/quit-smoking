e70aa0587ebdc18d32b8b80abbe8891696e13a29
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-06-20 20:33:06 +0200

    create a stepper for setup

---
+++ src/components/Setup/Congrats.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="congrats">
+    <p>Congratulations!</p>
+    <p>You are now a non-smoker</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // setTimeout(() => {
+    //   this.$router.push('/')
+    // }, 3000)
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.congrats {
+  text-align: center;
+}
+</style>
--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -1,23 +1,63 @@
 <template>
   <transition name="slide">
     <div class="setup">
-      <p>Please fill out the form to get started.</p>
-      <the-form  @onSubmit="saveSettings($event)"/>
+      <div class="stepper">
+        <div
+          :class="{
+            'stepper__tab': true,
+            'stepper__tab--active': step === 1,
+            'stepper__tab--complete': step > 1
+          }"
+        >
+          <div
+            :class="{
+              'stepper__icon': true,
+              'stepper__icon--active': step === 1,
+              'stepper__icon--complete': step > 1
+            }"
+          >1</div>
+        </div>
+        <div
+          :class="{
+            'stepper__tab': true,
+            'stepper__tab--active': step === 2,
+            'stepper__tab--complete': step > 2
+          }"
+        >
+          <div
+            :class="{
+              'stepper__icon': true,
+              'stepper__icon--active': step === 2,
+              'stepper__icon--complete': step > 2
+            }"
+          >1</div>
+        </div>
+      </div>
+      <component :is="currentComponent"  @onSubmit="saveSettings($event)"/>
     </div>
   </transition>
 </template>
 
 <script>
 import TheForm from "@/components/Settings/TheForm";
+import Congrats from "@/components/Setup/Congrats";
 
 export default {
   components: {
-    TheForm
+    TheForm,
+    Congrats
+  },
+  data() {
+    return {
+      currentComponent: "TheForm",
+      step: 1
+    };
   },
   methods: {
     saveSettings(settings) {
       this.$store.dispatch("saveSettings", settings);
-      this.$router.push("/");
+      this.currentComponent = "Congrats";
+      this.step++;
     }
   }
 };
@@ -30,5 +70,76 @@ export default {
   height: 100%;
   background-color: $color-primary;
   position: absolute;
+  z-index: 1;
+}
+
+.stepper {
+  background-color: orangered;
+  display: flex;
+  height: 5rem;
+
+  &__tab {
+    flex: 1 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &::before {
+      position: absolute;
+      left: 0;
+      content: "";
+      width: 50%;
+      height: 2px;
+      display: block;
+      background-color: #fff;
+    }
+
+    &::after {
+      position: absolute;
+      right: 0;
+      content: "";
+      width: 50%;
+      height: 2px;
+      display: block;
+      background-color: #fff;
+    }
+
+    &--complete {
+      &::before {
+        background-color: $color-green;
+      }
+
+      &::after {
+        background-color: $color-green;
+      }
+    }
+
+    &--active {
+      &::before {
+        background-color: $color-green;
+      }
+    }
+  }
+
+  &__icon {
+    background-color: #fff;
+    width: 3rem;
+    height: 3rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: 1;
+
+    &--complete {
+      background-color: $color-green;
+    }
+
+    &--active {
+      border: 2px solid $color-green;
+    }
+  }
 }
 </style>
