e6b1abcd360cf4dd07a134f9e326f7313dcd19c6
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-06-20 16:28:22 +0200

    add form to setup view

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Hello world!</div>
+  <div class="setup">
+    <p>Please fill out the form to get started.</p>
+    <the-form  @onSubmit="saveSettings($event)"/>
+  </div>
 </template>
+
+<script>
+import TheForm from "@/components/Settings/TheForm";
+
+export default {
+  components: {
+    TheForm
+  },
+  methods: {
+    saveSettings(settings) {
+      this.$store.dispatch("saveSettings", settings);
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.setup {
+  width: 100%;
+  height: 100%;
+  background-color: $color-primary;
+  position: absolute;
+}
+</style>
