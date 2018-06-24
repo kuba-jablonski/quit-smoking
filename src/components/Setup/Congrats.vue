bd2d890084d3a0eedf461b5d9bb15239de963288
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-06-24 14:56:02 +0200

    style congrats component

--- src/components/Setup/Congrats.vue
+++ src/components/Setup/Congrats.vue
@@ -1,22 +1,49 @@
 <template>
   <div class="congrats">
-    <p>Congratulations!</p>
-    <p>You are now a non-smoker</p>
+    <radial-progress-bar
+      :diameter="diameter"
+      :completed-steps="completedSteps"
+      :total-steps="totalSteps"
+      :animate-speed="3000"
+      start-color="#1E88E5"
+      stop-color="#1E88E5"
+      inner-stroke-color="#BDBDBD"
+    >
+      <p class="c-primary bold">Congratulations!</p>
+      <p>You are now a non-smoker.</p>
+    </radial-progress-bar>
+
   </div>
 </template>
 
 <script>
+import RadialProgressBar from "vue-radial-progress";
+
 export default {
+  components: {
+    RadialProgressBar
+  },
+  data() {
+    return {
+      totalSteps: 1,
+      completedSteps: 0,
+      diameter: window.innerWidth * 0.8
+    };
+  },
   mounted() {
-    // setTimeout(() => {
-    //   this.$router.push('/')
-    // }, 3000)
+    setTimeout(() => {
+      this.completedSteps = 1;
+    }, 100);
+    setTimeout(() => {
+      this.$router.push("/");
+    }, 3500);
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .congrats {
-  text-align: center;
+  display: flex;
+  justify-content: center;
 }
 </style>
