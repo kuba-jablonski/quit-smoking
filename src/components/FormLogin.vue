7fb2d2cd5823918b7bac3c5739f025e3a53d304f
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-07-05 09:12:09 +0200

    add register form

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -1,17 +1,48 @@
 <template>
-  <base-form @submit="onSubmit">
-    <base-input v-model="email" id="email" required type="email">
-      Email
-    </base-input>
-    <base-input v-model="password" required id="password">
-      Password
-    </base-input>
-    <div class="d-flex mt-md">
-      <slot name="form-settings-buttons">
-        <base-button class="form-btn" type="submit" color="primary" fullWidth>Save</base-button>
-      </slot>
+<div>
+  <transition name="fade" mode="out-in">
+    <div key="login" v-if="isLogin">
+      <p class="title mb-md text-center uppercase">Login</p>
+      <base-form @submit="onSubmit">
+        <base-input v-model="email" id="email" required type="email">
+          Email
+        </base-input>
+        <base-input v-model="password" required id="password">
+          Password
+        </base-input>
+        <div class="d-flex mt-md">
+          <slot name="form-settings-buttons">
+            <base-button class="form-btn" type="submit" color="primary" fullWidth>Save</base-button>
+          </slot>
+        </div>
+      </base-form>
+      <div @click="isLogin = false" class="register-link">
+        <p>Don't have an account?</p>
+        <p class="title link">Register now.</p>
+      </div>
     </div>
-  </base-form>
+    <div key="register" v-else>
+      <p class="title mb-md text-center uppercase">Register</p>
+      <base-form @submit="onSubmit">
+        <base-input v-model="email" id="email" required type="email">
+          Email
+        </base-input>
+        <base-input v-model="password" required id="password">
+          Password
+        </base-input>
+        <div class="d-flex mt-md">
+          <slot name="form-settings-buttons">
+            <base-button class="form-btn" type="submit" color="primary" fullWidth>Save</base-button>
+          </slot>
+        </div>
+      </base-form>
+      <div @click="isLogin = true" class="register-link">
+        <p>Already have an account?</p>
+        <p class="title link">Login now.</p>
+      </div>
+    </div>
+  </transition>
+</div>
 </template>
 
 <script>
@@ -19,7 +50,8 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      isLogin: true
     };
   },
   methods: {
@@ -48,3 +80,12 @@ export default {
   }
 };
 </script>
+
+
+<style lang="scss" scoped>
+.register-link {
+  margin-top: 3rem;
+  text-align: center;
+  cursor: pointer;
+}
+</style>
