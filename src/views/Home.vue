9d42e70d0eb71275b4f8aa40d971043dfbc6ab0d
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-07-19 14:59:26 +0200

    add author info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,6 +20,20 @@
           </router-link>
         </main>
       </base-container>
+      <div class="author">
+        <p class="author__text">Created by Jakub Jabłoński</p>
+        <div class="social">
+          <a href="#" class="social__link">
+            <settings-icon class="social__icon"/>
+          </a>
+          <a href="#" class="social__link">
+            <settings-icon class="social__icon"/>
+          </a>
+          <a href="#" class="social__link">
+            <settings-icon class="social__icon"/>
+          </a>
+        </div>
+      </div>
     </div>
   </transition>
 </template>
@@ -79,6 +93,36 @@ export default {
   grid-auto-rows: min-content;
   grid-gap: 1rem;
   padding: 1rem;
+}
+
+.author {
+  margin-top: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+
+  &__text {
+    margin-bottom: 0.5rem;
+    font-size: 2rem;
+    color: $color-grey-light-2;
+  }
+}
+
+.social {
+  display: flex;
+
+  &__link {
+    &:not(:last-child) {
+      margin-right: 4rem;
+    }
+  }
+
+  &__icon {
+    height: 4rem;
+    width: 4rem;
+    fill: $color-grey-light-2;
+  }
 }
 
 .router-link {
