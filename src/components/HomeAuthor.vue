d222e90b50c3387bcd4565c939be227c9ead7768
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-07-20 21:15:00 +0200

    add proper icons and links in home-author

--- src/components/HomeAuthor.vue
+++ src/components/HomeAuthor.vue
@@ -2,25 +2,29 @@
   <div class="author">
     <p class="author__text">Created by Jakub Jabłoński</p>
     <div class="social">
-      <a href="#" class="social__link">
-        <settings-icon class="social__icon"/>
+      <a href="https://jakub-jablonski.surge.sh" target="_blank" class="social__link">
+        <earth-icon class="social__icon"/>
       </a>
-      <a href="#" class="social__link">
-        <settings-icon class="social__icon"/>
+      <a href="https://github.com/kuba-jablonski" target="_blank" class="social__link">
+        <github-icon class="social__icon"/>
       </a>
-      <a href="#" class="social__link">
-        <settings-icon class="social__icon"/>
+      <a href="https://linkedin.com/in/jakub-jabłoński-47a990138/" target="_blank" class="social__link">
+        <linkedin-icon class="social__icon"/>
       </a>
     </div>
   </div>
 </template>
 
 <script>
-import SettingsIcon from "@/assets/svg/settings.svg";
+import EarthIcon from "@/assets/svg/earth.svg";
+import GithubIcon from "@/assets/svg/github.svg";
+import LinkedinIcon from "@/assets/svg/linkedin.svg";
 
 export default {
   components: {
-    SettingsIcon
+    EarthIcon,
+    GithubIcon,
+    LinkedinIcon
   }
 };
 </script>
@@ -36,8 +40,9 @@ export default {
 
   &__text {
     margin-bottom: 0.5rem;
-    font-size: 2rem;
+    font-size: 1.8rem;
     color: $color-grey-light-2;
+    border-bottom: 1px solid currentColor;
   }
 }
 
